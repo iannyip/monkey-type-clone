4c1d8ce2574bd7b61e61c5c7da7fdc89812b40c7
iannyip <ianyipsh@gmail.com>
2023-12-24 01:44:13 +0800

    bug fix - stop spacebar from toggling checkbox

--- src/components/guidedButton.jsx
+++ src/components/guidedButton.jsx
@@ -1,16 +1,32 @@
+import { useEffect } from 'react';
 import './styles.css';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 export default function GuidedButton({ guidedMode, updateGuidedMode }) {
+  // This useEffect handles the bug where spacebar toggles the checkbox. Looks for the checkbox and sets an event listener to disable (blur) it after it has been clicked
+  useEffect(() => {
+    const toggle = document.getElementById('visibility-toggle');
+
+    toggle.addEventListener('click', () => {
+      toggle.blur();
+    });
+    return () => {
+      document.removeEventListener('keydown', () => {
+        toggle.blur();
+      });
+    };
+  });
+
   const handleBtnToggle = (event) => {
+    console.log(event);
     updateGuidedMode(event.target.checked);
   };
 
   const visibilityOnStyle = {
     color: guidedMode ? '#FFFFFF' : '#808080',
     transition: '0.4s',
-    '-webkit-transition': '0.4s',
+    WebkitTransition: '0.4s',
   };
 
   const visibilityOffStyle = {
@@ -26,6 +42,7 @@ export default function GuidedButton({ guidedMode, updateGuidedMode }) {
       <label className='switch'>
         <input
           onChange={handleBtnToggle}
+          id='visibility-toggle'
           type='checkbox'
           defaultChecked='checked'
         />
